9b60c892cbd6ba6e0529fe39bd28407810383f32
Brendan Luke <31905278+BrendanLuke15@users.noreply.github.com>
2022-02-10 10:16:39 -0500

    accepts input args
    have a working Python method, this is likely now depricated

--- IMG_2_TXT.ps1
+++ IMG_2_TXT.ps1
@@ -2,9 +2,12 @@
 # Date: February 9, 2022
 # Purpose: Convert PDS .img files to .txt files
 
-# Specify Input & Output files
-$inFile = $PSScriptRoot + '\ldem_4.img'
-$outFile = $PSScriptRoot + '\LDEM_4.txt'
+Read-Host -Prompt "Press any key to continue"
+# Create Input & Output filepaths
+#$inFile = $PSScriptRoot + '\ldem_4.img'
+#$outFile = $PSScriptRoot + '\testLDEM_4.txt'
+$inFile = $PSScriptRoot + '\' + $Args[0]
+$outFile = $PSScriptRoot + '\' + $Args[1]
 
 format-hex $inFile > $outFile
 Read-Host -Prompt "Press any key to continue"
